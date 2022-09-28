65dead12cb2ed353d065d725014e664ec498179d
dashapakhutova <114560525+dashapakhutova@users.noreply.github.com>
2022-09-28 14:19:15 +0800

    Add files via upload

---
+++ ex3.ino
@@ -0,0 +1,21 @@
+#define sensor_pin A2
+void setup() {
+Serial.begin(9600);
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+int val = analogRead(sensor_pin);
+Serial.println(val);
+delay(10);
+if (val < 850){
+digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(1000);
+}
+else{
+digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+delay(1000);
+}
+// wait for a second
+
+}
