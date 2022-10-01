f995d5b079acd93b8db387157208acda2e2924f9
dashapakhutova <114560525+dashapakhutova@users.noreply.github.com>
2022-10-01 14:04:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.misc import face
-image = face(True)
+def lerp(v0, v1, t):
+    return (1 - t) * v0 + t * v1
 
-def block_mean(image,by_size=8,bx_size=8):
-    result = image.copy()
-    for y in range(int(image.shape[0]/ by_size)):
-        for x in range (int(image.shape[1]/ bx_size)):
-            sub = result[y*by_size:by_size*(y+1),x*bx_size:bx_size*(x+1)]
-            sub[:] = sub.mean()
-    return result
-    
+size = 100
+image = np.zeros((size, size, 3), dtype="uint8")
+assert image.shape[0] == image.shape[1]
 
-plt.subplot(121)
-plt.imshow(image, cmap="gray")
-plt.subplot(122)
-result = block_mean(image,16,16)
-plt.imshow(result, cmap="gray")
+color1 = [255, 128, 0]
+color2 = [0, 128, 255]
+
+for i, v in enumerate(np.linspace(0, 1, image.shape[0])):
+    for j, v2 in enumerate(np.linspace(0, 1, image.shape[1])):
+        r = lerp(color1[2], color2[2], (v + v2) / 2)
+        g = lerp(color1[1], color2[1], (v + v2) / 2)
+        b = lerp(color1[0], color2[0], (v + v2) / 2)
+        image[i, j, :] = [r, g, b]
+
+plt.figure(1)
+plt.imshow(image)
 plt.show()
